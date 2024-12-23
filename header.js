6799f83ad0abfa23cff164c16b1d82353550b4dc
Kalwakadi vikas <128769870+1010-vikas@users.noreply.github.com>
2024-12-23 14:25:25 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>CodeAnt AI</h1>
+      {/* Add your navigation items here */}
+    </header>
+  );
+}
+
+export default Header;
---
+++ homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function HomePage() {
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* Your main content goes here */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
